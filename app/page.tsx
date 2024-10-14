36f8c499dfb818897903d6f5c75bd33567b76cd9
Justas Samuolis <samuolis.just@gmail.com>
2024-10-14 11:40:05 +0300

    Update task 3

--- app/page.tsx
+++ app/page.tsx
@@ -38,13 +38,21 @@ export default function Home() {
       <hr className="my-2 h-0.5 border-t-0 bg-white/10" />
       <p>Task 3 time: </p>
       <code className="text-yellow-300">app/page.tsx</code>
+      <p>Handle the error response.</p>
+      <div className="p-4 my-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400">
+        <span className="font-bold">Tip!</span> Use <b>Record</b> type to map
+        response codes to messages
+      </div>
       <p>
-        Handle the error response.
-        <br />
-        Tip: Use <b>Record</b> type to map response codes to messages. <br />
         You can store it in &nbsp;
         <code className="text-yellow-300">app/constants/response.ts</code>
       </p>
+      <p>
+        Afterwards, if you are successful -- you should see error message after
+        few refreshes due to rate limits <br />
+        After you are done, you can move to the last task:
+      </p>
+      <code className="text-cyan-300">git checkout task4</code>
       {errorMessage ? (
         <p className="text-red-500 py-2">{errorMessage}</p>
       ) : null}
