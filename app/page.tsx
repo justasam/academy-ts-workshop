17342d013d1a1db63c1a1fcaf6b7c6ccc907cb59
Justas Samuolis <samuolis.just@gmail.com>
2024-10-14 11:08:54 +0300

    Update task 2

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,20 @@ export default function Home() {
       <h1 className="text-lg font-bold">Trivia Quiz</h1>
       <p className="text-slate-300">Welcome to the Trivia Quiz!</p>
       <hr className="my-2 h-0.5 border-t-0 bg-white/10" />
-      <p>Start by doing task 1: </p>
-      <code className="text-yellow-300">app/types/dtos/trivia-question.ts</code>
+      <p>Task 2 time: </p>
+      <code className="text-yellow-300">
+        app/data/transformers/trivia-question.ts
+      </code>
       <p>
-        Change the <b>type</b> of the &quot;type&quot; and
-        &quot;difficulty&quot; properties to an enums.
-      </p>
-      <p className="py-2">
-        Example of <b>enum</b> can be found in&nbsp;
-        <span className="font-mono text-yellow-300">
-          constants/response.ts
-        </span>{" "}
-        and&nbsp;
-        <span className="font-mono text-yellow-300">api/response.ts</span>.
+        Write a transformer function for the <b>TriviaQuestionDto</b>.
+        Afterwards,&nbsp;
+        <code className="text-yellow-300">
+          app/data/transformers/__tests__/trivia-question.test.ts
+        </code>
+        &nbsp; should pass.
       </p>
       <p className="pt-2">After you are done, checkout the next branch:</p>
-      <code className="text-teal-300">git checkout task2</code>
+      <code className="text-teal-300">git checkout task3</code>
     </main>
   );
 }
